85b9568b6ff744bda194aed3850ad9051952cc64
ZahiraPH <zahirapriyanhusna@gmail.com>
2025-06-14 14:48:49 +0700

    Zahira capek

--- src/main/java/org/example/smartmuseum/controller/LoginController.java
+++ src/main/java/org/example/smartmuseum/controller/LoginController.java
@@ -3,12 +3,12 @@ package org.example.smartmuseum.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.Node;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -22,22 +22,32 @@ public class LoginController {
 
     @FXML
     public void logginButton(ActionEvent event) {
-        System.out.println("Login attempted by: " + usernameTextField.getText());
-    }
+        // Ini hanya contoh, nanti bisa kamu isi dengan validasi username dan password
+        String username = usernameTextField.getText();
+        String password = enterPasswordField.getText();
 
-    @FXML
-    public void cancelButton(ActionEvent event) {
-        ((Stage)((Node)event.getSource()).getScene().getWindow()).close();
+        System.out.println("Login attempted by: " + username + " with password: " + password);
+
+        // Misalnya nanti kamu mau masuk ke halaman dashboard, bisa load fxml lain di sini
     }
 
     @FXML
     public void handleRegister(ActionEvent event) {
         try {
-            Parent registerRoot = FXMLLoader.load(getClass().getResource("/org/example/smartmuseum/view/register.fxml"));
+            // Load register.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/smartmuseum/view/register.fxml"));
+            Parent registerRoot = loader.load();
+
+            // Buka halaman register
             Stage registerStage = new Stage();
             registerStage.setTitle("SmartMuseum - Register");
             registerStage.setScene(new Scene(registerRoot));
             registerStage.show();
+
+            // Tutup halaman login
+            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            loginStage.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
